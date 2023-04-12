932a51733fc94c7ab86a70111ec6c347459e27a2
MaSedlacek <mareksedlacek@aol.com>
2023-04-12 22:18:28 +0200

    init

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, Inject, OnDestroy, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { Subscription, filter } from "rxjs";
+import { ProductService } from "./product.service";
+
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {    
+
+    constructor(private productService: ProductService) {}
+
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
+    
+    private _listFilter: string = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        console.log('In setter: ', value)
+        this.filteredProducts = this.performFilter(value)
+    }
+    filteredProducts: IProduct[] = [];
+
+    products: IProduct[] = [];
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().includes(filterBy))
+    }    
+
+    toggleImage() : void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;},
+            error: err => this.errorMessage = err
+        });        
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List ' + message;
+    }
+
+}
